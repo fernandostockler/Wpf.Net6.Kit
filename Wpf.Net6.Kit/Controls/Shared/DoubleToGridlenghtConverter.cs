28761f826e49f4a0cb0daa64d3a779249b241195
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-08-04 02:04:09 -0300

    Add DoubleToGridlenghtConverter

---
+++ Wpf.Net6.Kit/Controls/Shared/DoubleToGridlenghtConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wpf.Net6.Kit.Controls.Shared
+{
+    [ValueConversion(typeof(double), typeof(GridLength))]
+    public class DoubleToGridlenghtConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is double height) ? new GridLength(height) : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
