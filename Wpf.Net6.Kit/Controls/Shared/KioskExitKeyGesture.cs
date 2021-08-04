c521005e0f8f8cdfb8dca092d0510737b3d003d9
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-08-04 01:54:16 -0300

    Add KioskExitKeyGesture

---
+++ Wpf.Net6.Kit/Controls/Shared/KioskExitKeyGesture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Windows.Input;
+
+namespace Wpf.Net6.Kit.Controls.Shared
+{
+    /// <summary>
+    /// This class represents a keyboard key and its modifiers.
+    /// </summary>
+    public class KioskExitKeyGesture
+    {
+        public ModifierKeys[] ModifierKeys { get; private set; } = Array.Empty<ModifierKeys>();
+
+        public Key Key { get; private set; } = Key.None;
+
+        public KioskExitKeyGesture() { }
+
+        public KioskExitKeyGesture(Key key, ModifierKeys[] modifierKeys)
+        {
+            Key = key;
+            ModifierKeys = modifierKeys;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            foreach (ModifierKeys modifier in ModifierKeys)
+            {
+                _ = sb.Append(modifier.ToString());
+                _ = sb.Append(" + ");
+            }
+            _ = sb.Append(Key.ToString());
+            return sb.ToString();
+        }
+    }
+}
