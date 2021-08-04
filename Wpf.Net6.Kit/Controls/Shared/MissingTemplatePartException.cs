f2a0d91d10f63da982c78fa5eb013dd6caa336a1
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-08-04 01:50:08 -0300

    Add MissingTemplatePartException

---
+++ Wpf.Net6.Kit/Controls/Shared/MissingTemplatePartException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Wpf.Net6.Kit.Controls.Shared
+{
+    public class MissingTemplatePartException : Exception
+    {
+        public string PartName { get; private set; } = string.Empty;
+        public Type? PartType { get; private set; }
+
+        public MissingTemplatePartException() { }
+
+        public MissingTemplatePartException(string? message) : base(message) { }
+
+        public MissingTemplatePartException(string partName, Type partType)
+        {
+            PartName = partName;
+            PartType = partType;
+        }
+
+        public MissingTemplatePartException(string? message, Exception? innerException) : base(message, innerException) { }
+
+        protected MissingTemplatePartException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
