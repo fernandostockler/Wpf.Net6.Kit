1b82f29d3f893b7d497fc7c1867af0ee65c3d2f5
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-08-07 23:48:42 -0300

    Add some views

---
+++ Wpf.Net6.Kit.App/Views/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.Net6.Kit.App.Views
+{
+    /// <summary>
+    /// Interação lógica para Page2.xam
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Wpf.Net6.Kit.App/Views/UserControl2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.Net6.Kit.App.Views
+{
+    /// <summary>
+    /// Interação lógica para UserControl2.xam
+    /// </summary>
+    public partial class UserControl2 : UserControl
+    {
+        public UserControl2()
+        {
+            InitializeComponent();
+        }
+    }
+}
