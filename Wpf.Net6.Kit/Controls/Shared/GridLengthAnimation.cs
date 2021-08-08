ca86cc71e5c3f37568fd5382e0412173880f0314
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-08-07 23:32:33 -0300

    Add SideMenu and GridLengthAnimation

---
+++ Wpf.Net6.Kit/Controls/Shared/GridLengthAnimation.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Wpf.Net6.Kit.Controls.Shared
+{
+    /// <summary>
+    /// Animates a grid length value just like the DoubleAnimation animates a double value
+    /// </summary>
+    public class GridLengthAnimation : AnimationTimeline
+    {
+        private bool isCompleted;
+
+        /// <summary>
+        /// Marks the animation as completed
+        /// </summary>
+        public bool IsCompleted
+        {
+            get => isCompleted;
+            set => isCompleted = value;
+        }
+
+        /// <summary>
+        /// Sets the reverse value for the second animation
+        /// </summary>
+        public double ReverseValue
+        {
+            get => (double)GetValue(ReverseValueProperty);
+            set => SetValue(ReverseValueProperty, value);
+        }
+        /// <summary>
+        /// Dependency property. Sets the reverse value for the second animation
+        /// </summary>
+        public static readonly DependencyProperty ReverseValueProperty =
+            DependencyProperty.Register(
+                name: nameof(ReverseValue),
+                propertyType: typeof(double),
+                ownerType: typeof(GridLengthAnimation),
+                typeMetadata: new UIPropertyMetadata(
+                    defaultValue: 0.0));
+
+        /// <summary>
+        /// CLR Wrapper for the From depenendency property
+        /// </summary>
+        public GridLength From
+        {
+            get => (GridLength)GetValue(FromProperty);
+            set => SetValue(FromProperty, value);
+        }
+        /// <summary>
+        /// Dependency property for the From property
+        /// </summary>
+        public static readonly DependencyProperty FromProperty =
+            DependencyProperty.Register(
+                name: nameof(From),
+                propertyType: typeof(GridLength),
+                ownerType: typeof(GridLengthAnimation));
+
+        /// <summary>
+        /// CLR Wrapper for the To property
+        /// </summary>
+        public GridLength To
+        {
+            get => (GridLength)GetValue(ToProperty);
+            set => SetValue(ToProperty, value);
+        }
+        /// <summary>
+        /// Dependency property for the To property
+        /// </summary>
+        public static readonly DependencyProperty ToProperty =
+            DependencyProperty.Register(
+                name: nameof(To),
+                propertyType: typeof(GridLength),
+                ownerType: typeof(GridLengthAnimation));
+
+        /// <summary>
+        /// Returns the type of object to animate
+        /// </summary>
+        public override Type TargetPropertyType => typeof(GridLength);
+
+        /// <summary>
+        /// Creates an instance of the animation object
+        /// </summary>
+        /// <returns>Returns the instance of the GridLengthAnimation</returns>
+        protected override Freezable CreateInstanceCore() => new GridLengthAnimation();
+
+        /// <summary>
+        /// Animates the grid let set
+        /// </summary>
+        /// <param name=”defaultOriginValue”>The original value to animate</param>
+        /// <param name=”defaultDestinationValue”>The final value</param>
+        /// <param name=”animationClock”>The animation clock (timer)</param>
+        /// <returns>Returns the new grid length to set</returns>
+        public override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock)
+        {
+            //check the animation clock event
+            VerifyAnimationCompletedStatus(animationClock);
+
+            //check if the animation was completed
+            if (isCompleted)
+            {
+                return (GridLength)defaultDestinationValue;
+            }
+
+            //if not then create the value to animate
+            double fromVal = From.Value;
+            double toVal = To.Value;
+
+            //check if the value is already collapsed
+            if (((GridLength)defaultOriginValue).Value == toVal)
+            {
+                fromVal = toVal;
+                toVal = ReverseValue;
+            }
+            //check to see if this is the last tick of the animation clock.
+#pragma warning disable CS8629 // Nullable value type may be null.
+            else if (animationClock.CurrentProgress.Value == 1.0)
+#pragma warning restore CS8629 // Nullable value type may be null.
+            {
+                return To;
+            }
+
+            double doubleValue = fromVal > toVal
+#pragma warning disable CS8629 // Nullable value type may be null.
+                ? ((1.0 - animationClock.CurrentProgress.Value) * (fromVal - toVal)) + toVal
+                : (animationClock.CurrentProgress.Value * (toVal - fromVal)) + fromVal;
+#pragma warning restore CS8629 // Nullable value type may be null.
+
+            return new GridLength(doubleValue, From.IsStar ? GridUnitType.Star : GridUnitType.Pixel);
+        }
+
+        private AnimationClock? clock;
+
+        /// <summary>
+        /// registers to the completed event of the animation clock
+        /// </summary>
+        /// <param name=”clock”>the animation clock to notify completion status</param>
+        private void VerifyAnimationCompletedStatus(AnimationClock clock)
+        {
+            if (this.clock == null)
+            {
+                this.clock = clock;
+                this.clock.Completed += (_, _) => isCompleted = true;
+            }
+        }
+    }
+}
---
+++ Wpf.Net6.Kit/Controls/SideMenu.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Net6.Kit.Controls.Shared;
+
+namespace Wpf.Net6.Kit.Controls
+{
+    [TemplatePart(Name = PART_GridSplitter, Type = typeof(GridSplitter))]
+    [TemplatePart(Name = PART_LeftPanelColumn, Type = typeof(ColumnDefinition))]
+    [TemplatePart(Name = PART_HamburgerButton, Type = typeof(ButtonBase))]
+    [TemplatePart(Name = PART_Frame, Type = typeof(Frame))]
+    public class SideMenu : ListBox
+    {
+        private const string Comum = "Comum";
+        private const string NoItemIsSelected = "NoItemIsSelected";
+        private readonly TextBlock BackgroundPageTextBlock = new()
+        {
+            Text = "Use DefaultBackgroundPage property to set this page with any FrameworkElement.",
+            FontSize = 18,
+            Foreground = Brushes.Violet,
+            Padding = new Thickness(50, 20, 50, 20),
+            TextAlignment = TextAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center,
+            TextWrapping = TextWrapping.Wrap
+        };
+        private readonly TextBlock PageNotFounded = new()
+        {
+            Text = "Page not founded!",
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center,
+            FontSize = 42.0
+        };
+
+        static SideMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(forType: typeof(SideMenu), typeMetadata: new FrameworkPropertyMetadata(typeof(SideMenu)));
+        }
+
+        public SideMenu()
+        {
+            _ = CommandBindings.Add(new CommandBinding(
+                command: TogglePanelCommand,
+                executed: TogglePanelCommandExecuted,
+                canExecute: TogglePanelCommandCanExecute));
+
+            BackgroundPage = BackgroundPageTextBlock;
+            Loaded += SideMenu_Loaded;
+            SelectionChanged += SideMenu_SelectionChanged;
+        }
+
+        public static readonly RoutedCommand TogglePanelCommand = new(
+            name: nameof(TogglePanelCommand),
+            ownerType: typeof(SideMenu),
+            inputGestures: new(new List<KeyGesture>() { new(Key.Space, ModifierKeys.Shift) }));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um dicionário com as páginas (qualquer FrameWorkElement) para navegação usado pelo Frame interno.")]
+        public Dictionary<string, FrameworkElement> Pages
+        {
+            get => (Dictionary<string, FrameworkElement>)GetValue(PagesProperty);
+            set => SetValue(PagesProperty, value);
+        }
+        public static readonly DependencyProperty PagesProperty =
+            DependencyProperty.Register(
+                name: nameof(Pages),
+                propertyType: typeof(Dictionary<string, FrameworkElement>),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: new Dictionary<string, FrameworkElement>()));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um FrameworkElement que representa a página de fundo da aplicação, que será exibida se nenhum item estiver selecionado ou se a propriedade Pages não contiver nenhuma entrada.")]
+        public FrameworkElement BackgroundPage
+        {
+            get => (FrameworkElement)GetValue(BackgroundPageProperty);
+            set => SetValue(BackgroundPageProperty, value);
+        }
+        public static readonly DependencyProperty BackgroundPageProperty =
+            DependencyProperty.Register(
+                name: nameof(BackgroundPage),
+                propertyType: typeof(FrameworkElement),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(defaultValue: null));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um valor que representa a visibilidade da barra de navegação do frame.")]
+        public NavigationUIVisibility NavigationUIVisibility
+        {
+            get => (NavigationUIVisibility)GetValue(NavigationUIVisibilityProperty);
+            set => SetValue(NavigationUIVisibilityProperty, value);
+        }
+        public static readonly DependencyProperty NavigationUIVisibilityProperty =
+            DependencyProperty.Register(
+                name: nameof(NavigationUIVisibility),
+                propertyType: typeof(NavigationUIVisibility),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(NavigationUIVisibility.Hidden));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um FrameworkElement personalizado localizado na parte inferior esquerda do controle.")]
+        public FrameworkElement CustomArea
+        {
+            get => (FrameworkElement)GetValue(CustomAreaProperty);
+            set => SetValue(CustomAreaProperty, value);
+        }
+        public static readonly DependencyProperty CustomAreaProperty =
+            DependencyProperty.Register(
+                name: nameof(CustomArea),
+                propertyType: typeof(FrameworkElement),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: null));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um valor que representa a parte sempre visível do painel esquerdo.")]
+        public double MenuMinWidth
+        {
+            get => (double)GetValue(MenuMinWidthProperty);
+            set => SetValue(MenuMinWidthProperty, value);
+        }
+        public static readonly DependencyProperty MenuMinWidthProperty =
+            DependencyProperty.Register(
+                name: nameof(MenuMinWidth),
+                propertyType: typeof(double),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: 55.0));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um valor que representa a largura máxima do painel esquerdo.")]
+        public double MenuMaxWidth
+        {
+            get => (double)GetValue(MenuMaxWidthProperty);
+            set => SetValue(MenuMaxWidthProperty, value);
+        }
+        public static readonly DependencyProperty MenuMaxWidthProperty =
+            DependencyProperty.Register(
+                name: nameof(MenuMaxWidth),
+                propertyType: typeof(double),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: 320.0));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um valor que representa a largura do painel esquerdo.")]
+        public double MenuWidth
+        {
+            get => (double)GetValue(MenuWidthProperty);
+            set => SetValue(MenuWidthProperty, value);
+        }
+        public static readonly DependencyProperty MenuWidthProperty =
+            DependencyProperty.Register(
+                name: nameof(MenuWidth),
+                propertyType: typeof(double),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: 320.0));
+
+        [Description("Obtem ou define um pincel que descreve a cor do primeiro plano do botão que abre/fecha o painel esquerdo.")]
+        public Brush HamburgerMenuForeground
+        {
+            get => (Brush)GetValue(HamburgerMenuForegroundProperty);
+            set => SetValue(HamburgerMenuForegroundProperty, value);
+        }
+        public static readonly DependencyProperty HamburgerMenuForegroundProperty =
+            DependencyProperty.Register(
+                name: nameof(HamburgerMenuForeground),
+                propertyType: typeof(Brush),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: Brushes.GreenYellow));
+
+        [Description("Obtem ou define um pincel que descreve a cor do GridSplitter.")]
+        public Brush GridSplitterBackground
+        {
+            get => (Brush)GetValue(GridSplitterBackgroundProperty);
+            set => SetValue(GridSplitterBackgroundProperty, value);
+        }
+        public static readonly DependencyProperty GridSplitterBackgroundProperty =
+            DependencyProperty.Register(
+                name: nameof(GridSplitterBackground),
+                propertyType: typeof(Brush),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: Brushes.Transparent));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um valor que representa a espessura da GridSplitter.")]
+        public double GridSplitterWidth
+        {
+            get => (double)GetValue(GridSplitterWidthProperty);
+            set => SetValue(GridSplitterWidthProperty, value);
+        }
+        public static readonly DependencyProperty GridSplitterWidthProperty =
+            DependencyProperty.Register(
+                name: nameof(GridSplitterWidth),
+                propertyType: typeof(double),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: 1.0));
+
+        [Category(Comum)]
+        [Description("Obtem ou define um valor que representa se o GridSplitter está habilitado.")]
+        public bool GridSplitterIsEnabled
+        {
+            get => (bool)GetValue(GridSplitterIsEnabledProperty);
+            set => SetValue(GridSplitterIsEnabledProperty, value);
+        }
+        public static readonly DependencyProperty GridSplitterIsEnabledProperty =
+            DependencyProperty.Register(
+                name: nameof(GridSplitterIsEnabled),
+                propertyType: typeof(bool),
+                ownerType: typeof(SideMenu),
+                typeMetadata: new PropertyMetadata(
+                    defaultValue: false));
+
+        private void SideMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            SideMenuItem sideMenuItem = (SideMenuItem)SelectedItem;
+            string pageInfo = SelectedIndex > -1 ? sideMenuItem.PageTypeName : NoItemIsSelected;
+            SelectPage(pageInfo);
+        }
+
+        private void SideMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0 && e.AddedItems[0] is SideMenuItem item)
+            {
+                SelectPage(item.PageTypeName);
+            }
+        }
+
+        private void TogglePanelCommandCanExecute(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
+
+        private void TogglePanelCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            double oldValue = MenuWidth;
+            MenuWidth = MenuWidth > MenuMinWidth ? MenuMinWidth : ActualMenuMaxWidth;
+            OnPropertyChanged(new(MenuWidthProperty, oldValue, MenuWidth));
+            e.Handled = true;
+
+            GridLengthAnimation? animation = new()
+            {
+                From = new GridLength(oldValue, GridUnitType.Pixel),
+                To = new GridLength(MenuWidth, GridUnitType.Pixel),
+                Duration = new(TimeSpan.FromSeconds(0.15)),
+                AccelerationRatio = 0.4,
+                DecelerationRatio = 0.6
+            };
+
+            Storyboard? storyboard = new();
+            storyboard.Children.Add(animation);
+            Storyboard.SetTargetName(animation, PartLeftPanelColumn.Name);
+            PropertyPath propertyPath = new(ColumnDefinition.WidthProperty);
+            Storyboard.SetTargetProperty(animation, propertyPath);
+            storyboard.Begin(containingObject: PartLeftPanelColumn);
+        }
+
+        private void GridSplitter_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            double oldValue = MenuWidth;
+            MenuWidth = oldValue + e.HorizontalChange;
+            OnPropertyChanged(new(MenuWidthProperty, oldValue, MenuWidth));
+            ActualMenuMaxWidth = MenuWidth;
+            e.Handled = true;
+        }
+
+        private void SelectPage(string pageTypeName)
+        {
+            if (IsLoaded)
+            {
+                bool hasNoSelectedItems = !string.IsNullOrEmpty(pageTypeName) && pageTypeName == NoItemIsSelected;
+                PartFrame.Content = hasNoSelectedItems || Pages.Count == 0
+                    ? BackgroundPage : Pages.ContainsKey(pageTypeName)
+                    ? Pages[pageTypeName] : PageNotFounded;
+            }
+        }
+
+        private T GetTemplateChild<T>(string childName) where T : DependencyObject
+        {
+            T child = (T)GetTemplateChild(childName);
+            return child is null ? throw new MissingTemplatePartException(childName, typeof(T)) : child;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            PartFrame = GetTemplateChild<Frame>(PART_Frame);
+            PartLeftPanelColumn = GetTemplateChild<ColumnDefinition>(PART_LeftPanelColumn);
+            PartGridSplitter = GetTemplateChild<GridSplitter>(PART_GridSplitter);
+            PartGridSplitter.DragCompleted -= GridSplitter_DragCompleted;
+            PartGridSplitter.DragCompleted += GridSplitter_DragCompleted;
+        }
+
+        protected override DependencyObject GetContainerForItemOverride() => new SideMenuItem();
+        protected override bool IsItemItsOwnContainerOverride(object item) => item is SideMenuItem;
+
+        private const string PART_GridSplitter = "PART_GridSplitter";
+        private const string PART_LeftPanelColumn = "PART_LeftPanelColumn";
+        private const string PART_HamburgerButton = "PART_HamburgerButton";
+        private const string PART_Frame = "PART_Frame";
+
+        internal GridSplitter PartGridSplitter { get; set; } = new();
+        internal ColumnDefinition PartLeftPanelColumn { get; set; } = new();
+        internal Frame PartFrame { get; set; } = new();
+        internal double ActualMenuMaxWidth { get; set; } = 320.0;
+    }
+}
