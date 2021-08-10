8c319348852320ebce81e0e03a2a82a8f1e9e340
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-08-10 00:54:38 -0300

    Add mvvm classes

---
+++ Wpf.Net6.Kit/Mvvm/NotifyPropertyChangedBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Wpf.Net6.Kit.Mvvm
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Wpf.Net6.Kit/Mvvm/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Wpf.Net6.Kit.Mvvm
+{
+    public abstract class ViewModelBase : NotifyPropertyChangedBase
+    {
+        protected ViewModelBase() { }
+
+        private string _title = string.Empty;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
