14ecd65fff08b7e4a2e8025ab5c8f8b9bd1af994
Fernando Stockler de Lima <fernando.stockler@outlook.com.br>
2021-08-13 18:28:50 -0300

    documentation added

--- Wpf.Net6.Kit/Mvvm/NotifyPropertyChangedBase.cs
+++ Wpf.Net6.Kit/Mvvm/NotifyPropertyChangedBase.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Wpf.Net6.Kit.Mvvm
@@ -37,5 +38,58 @@ namespace Wpf.Net6.Kit.Mvvm
         /// </summary>
         /// /// <param name="propertyName">(optional) The name of the property that is changing.</param>
         protected virtual void NotifyPropertyChanging([CallerMemberName] string? propertyName = "") => NotifyPropertyChanging(new PropertyChangingEventArgs(propertyName));
+
+        /// <summary>
+        /// Compares the property's stored value with the new entered value. 
+        /// If they are equal, false is returned, otherwise the stored value is updated with the new value, 
+        /// PropertyChanging, PropertyChanged events are raised, and true is returned.
+        /// </summary>
+        /// <typeparam name="T">The type of property that is changing.</typeparam>
+        /// <param name="storage">The currently stored value.</param>
+        /// <param name="newValue">The updated value for this property.</param>
+        /// <param name="propertyName">The property name (optional).</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        protected virtual bool SetProperty<T>(ref T storage, T newValue, [CallerMemberName] string? propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, newValue))
+            {
+                return false;
+            }
+
+            NotifyPropertyChanging(propertyName);
+
+            storage = newValue;
+
+            NotifyPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Compares the property's stored value with the new entered value. 
+        /// If they are equal, false is returned, otherwise the stored value is updated with the new value, 
+        /// PropertyChanging, PropertyChanged events are raised, and true is returned.
+        /// </summary>
+        /// <typeparam name="T">The type of property that is changing.</typeparam>
+        /// <param name="storage">The currently stored value.</param>
+        /// <param name="newValue">The updated value for this property.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> instance to use to compare the input values.</param>
+        /// <param name="propertyName">The property name (optional).</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        protected virtual bool SetProperty<T>(ref T storage, T newValue, IEqualityComparer<T> comparer, [CallerMemberName] string? propertyName = null)
+        {
+            if (comparer.Equals(storage, newValue))
+            {
+                return false;
+            }
+
+            NotifyPropertyChanging(propertyName);
+
+            storage = newValue;
+
+            NotifyPropertyChanged(propertyName);
+
+            return true;
+        }
     }
 }
--- Wpf.Net6.Kit/Mvvm/ViewModelBase.cs
+++ Wpf.Net6.Kit/Mvvm/ViewModelBase.cs
@@ -3,6 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace Wpf.Net6.Kit.Mvvm
 {
+    /// <summary>
+    /// A base class for viewmodels that allows their properties to be observable.
+    /// </summary>
     public abstract class ViewModelBase : NotifyPropertyChangedBase
     {
         protected ViewModelBase() { }
@@ -13,39 +16,6 @@ namespace Wpf.Net6.Kit.Mvvm
         {
             get => _title;
             set => SetProperty(ref _title, value);
-        }
-
-        protected virtual bool SetProperty<T>(ref T storage, T newValue, [CallerMemberName] string? propertyName = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, newValue))
-            {
-                return false;
-            }
-
-            NotifyPropertyChanging(propertyName);
-
-            storage = newValue;
-
-            NotifyPropertyChanged(propertyName);
-
-            return true;
-        }
-
-
-        protected virtual bool SetProperty<T>(ref T storage, T newValue, IEqualityComparer<T> comparer, [CallerMemberName] string? propertyName = null)
-        {
-            if (comparer.Equals(storage, newValue))
-            {
-                return false;
-            }
-
-            NotifyPropertyChanging(propertyName);
-
-            storage = newValue;
-
-            NotifyPropertyChanged(propertyName);
-
-            return true;
         }
     }
 }
